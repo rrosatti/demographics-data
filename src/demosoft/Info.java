e68b472feae1c4e94668c87a3913c66343c9972f
Luis <Luis@Ghost>
2016-08-30 14:46:27 -0300

    Classe Info(reporting methods)

---
+++ src/demosoft/Info.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demosoft;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Luis
+ */
+public class Info {
+    private String countryCode;
+    private String countryName;
+    private String topic;
+    private int startYear;
+    private int endYear;
+    private HashMap<Integer,String> results;
+    
+   
+}
+   
+    
+

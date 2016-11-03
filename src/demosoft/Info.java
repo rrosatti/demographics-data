5439626487c7ab5357d79f4e7336c2715e8933b1
Tiago Alves da Silva <tiagorts1996@hotmail.com>
2016-11-03 13:25:03 -0200

    Revert "Organisação do Projeto em pacotes no padrão MVC"
    This reverts commit 8db01ceffe13d9e8e434a6bcde133041908bbdb2.

--- src/demosoft/Info.java
+++ src/demosoft/Info.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package demosoft.ApplicationLogic;
+package demosoft;
 
 import java.util.HashMap;
 
